198d8092f356cb8bc0ee1d8550b9213a4e10fa96
YBFJ <minjiachen182@163.com>
2018-09-23 12:59:59 +0800

    project initial staff

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+// 此为项目的入口文件
+import Vue from 'vue';
+import App from './App';
+import router from './router';
+import fastClick from 'fastclick';
+import './assets/styles/reset.css';
+// 使得所有设备上显示的是一样的
+import './assets/styles/border.css';
+// 使2k屏幕上正常显示，因为像素和我们定义的像素不一样了
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+fastClick.attach(document.body);
 
 /* eslint-disable no-new */
 new Vue({
@@ -12,4 +19,4 @@ new Vue({
   router,
   components: { App },
   template: '<App/>'
-})
+});
--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -1,12 +1,14 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+<div>
+  <div>home</div>
+
+</div>
+
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'Home'
 };
 </script>
 
