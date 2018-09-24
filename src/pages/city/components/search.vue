389a9884205a3e874f50e78c033e390c460984b0
YBFJ <minjiachen182@163.com>
2018-09-24 22:37:35 +0800

    city search box

---
+++ src/pages/city/components/search.vue
@@ -0,0 +1,32 @@
+<template>
+
+  <div class="search">
+    <input class="search-input" type="text" placeholder="输入城市名字或者拼音">
+    <router-link to="/">
+      <div class="iconfont header-back">&#xe624;</div>
+    </router-link>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'CitySearch'
+};
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibels.styl'
+.search
+  height 0.72rem
+  padding 0.1 rem
+  background $bgColor
+  .search-input
+    box-sizing border-box
+    width 100%
+    height 0.62rem
+    padding 0 0.1rem
+    line-height 0.62rem
+    text-align center
+    border-radius 0.06rem
+    color #666
+</style>
